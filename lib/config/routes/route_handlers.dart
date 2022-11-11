2b462f9b0bc42217d04b97286b3a45b5c83f411a
behrooz azimifar <behrooz2546@gmail.com>
2022-11-11 13:08:53 +0330

    add customer bloc to service locator

--- lib/config/routes/route_handlers.dart
+++ lib/config/routes/route_handlers.dart
@@ -2,6 +2,7 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mc_crud_test/config/routes/arguments.dart';
+import 'package:mc_crud_test/features/customer/presentation/bloc/customer/customer_bloc.dart';
 import 'package:mc_crud_test/features/customer/presentation/bloc/customer_list/customer_list_bloc.dart';
 import 'package:mc_crud_test/features/customer/presentation/pages/customer_form_page.dart';
 import 'package:mc_crud_test/features/customer/presentation/pages/customer_list_page.dart';
@@ -23,5 +24,8 @@ var customerListHanlder = Handler(
 var customerFormHanlder = Handler(
     handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
   final args = context?.settings?.arguments as CustomerFormPageArguments;
-  return CustomerFormPage(arguments: args);
+  return BlocProvider(
+    create: (context) => sl<CustomerBloc>(),
+    child: CustomerFormPage(arguments: args),
+  );
 });
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -3,7 +3,11 @@ import 'package:mc_crud_test/features/customer/data/repositories/customer_databa
 import 'package:mc_crud_test/features/customer/data/repositories/customer_repository_impl.dart';
 import 'package:mc_crud_test/features/customer/domain/repositories/customer_database_service.dart';
 import 'package:mc_crud_test/features/customer/domain/repositories/get_customer_repository.dart';
+import 'package:mc_crud_test/features/customer/domain/usecases/create_customer_usecase.dart';
+import 'package:mc_crud_test/features/customer/domain/usecases/delete_customer_usecase.dart';
 import 'package:mc_crud_test/features/customer/domain/usecases/get_all_customers_usecase.dart';
+import 'package:mc_crud_test/features/customer/domain/usecases/update_customer_usecase.dart';
+import 'package:mc_crud_test/features/customer/presentation/bloc/customer/customer_bloc.dart';
 import 'package:mc_crud_test/features/customer/presentation/bloc/customer_list/customer_list_bloc.dart';
 
 // ServiceLocator
@@ -12,9 +16,23 @@ final sl = GetIt.instance;
 Future<void> init() async {
   // blocs --> Factory
   sl.registerFactory<CustomerListBloc>(() => CustomerListBloc(sl()));
+  sl.registerFactory<CustomerBloc>(
+    () => CustomerBloc(
+      createCustomerUsecase: sl(),
+      deleteCustomerUsecase: sl(),
+      updateCustomerUsecase: sl(),
+    ),
+  );
 
   // UseCases --> LazySingleton
-  sl.registerLazySingleton(() => GetAllCustomersUsecase(sl()));
+  sl.registerLazySingleton<GetAllCustomersUsecase>(
+      () => GetAllCustomersUsecase(sl()));
+  sl.registerLazySingleton<CreateCustomerUsecase>(
+      () => CreateCustomerUsecase(sl()));
+  sl.registerLazySingleton<UpdateCustomerUsecase>(
+      () => UpdateCustomerUsecase(sl()));
+  sl.registerLazySingleton<DeleteCustomerUsecase>(
+      () => DeleteCustomerUsecase(sl()));
 
   // Repositories
   sl.registerLazySingleton<GetCustomerRepository>(
