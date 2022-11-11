c7a53d5fefec43cefa66193875140342b54c338a
behrooz azimifar <behrooz2546@gmail.com>
2022-11-11 12:20:45 +0330

    write test for update customer usecase

---
+++ test/features/customer/domain/usecases/update_customer_test.dart
@@ -0,0 +1,41 @@
+import 'package:either_dart/either.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mc_crud_test/features/customer/data/datasources/update_customer_request.dart';
+import 'package:mc_crud_test/features/customer/data/models/customer_model.dart';
+import 'package:mc_crud_test/features/customer/domain/repositories/update_customer_repository.dart';
+import 'package:mc_crud_test/features/customer/domain/usecases/update_customer_usecase.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'update_customer_test.mocks.dart';
+
+@GenerateMocks([
+  UpdateCustomerRepository,
+  CustomerModel,
+  UpdateCustomerRequest,
+])
+void main() async {
+  late UpdateCustomerUsecase usecase;
+  late MockUpdateCustomerRepository repository;
+
+  setUp(() {
+    repository = MockUpdateCustomerRepository();
+    usecase = UpdateCustomerUsecase(repository);
+  });
+
+  final customer = MockCustomerModel();
+  final request = MockUpdateCustomerRequest();
+
+  test('should create customer from repository', () async {
+    when(repository.updateCustomer(any))
+        .thenAnswer((_) async => Right(customer));
+    final result = await usecase.call(request);
+
+    expect(result, Right(customer));
+
+    verify(repository.updateCustomer(request));
+    verifyNoMoreInteractions(repository);
+  });
+
+  tearDown(() {});
+}
